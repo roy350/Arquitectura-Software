9f7e1b426d32d9afcbe6db524c2ef71eaa8589ff
Rodrigo Cortes <racortes2@uc.cl>
2018-09-03 12:46:49 -0300

    change other wgsi

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 
 import os
 
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
-from helloworld.wsgi import HelloWorldApplication
-application = HelloWorldApplication(application)
+application = get_wsgi_application()
