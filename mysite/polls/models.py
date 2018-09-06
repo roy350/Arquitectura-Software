e7411e5b6c7f6e5e04a8c93f7225ee5020cdc2d3
Rodrigo Cortes <racortes2@uc.cl>
2018-09-05 21:41:22 -0300

    change pub_date model

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 class Message(models.Model):
     text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField(auto_now_add=True)
+    pub_date = models.DateTimeField()
     ip = models.CharField(max_length=200)
